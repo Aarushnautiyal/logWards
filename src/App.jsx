9b28ef02cb1c94111deaa59f9805058113608766
Aarushnautiyal <sandeepnautiyal1115@gmail.com>
2023-12-09 10:56:06 +0530

    date-09-12: Initial commit with coment box styling

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import CommentCreationBox from "./components/CommentCreationBox"
+
+const App = () => {
+
+   
+  return (
+   <div className="container">
+     <CommentCreationBox/>
+   </div>
+
+  )
+}
+
+export default App
---
+++ src/components/CommentCreationBox.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+// import { changeHandler, submitHandler } from '../utility/utils'
+import styles from "./comment.module.scss"
+
+const CommentCreationBox = () => {
+    const [formData,setFormData] = useState({
+        userName:"",
+        userComment:""
+    });
+     const submitHandler =(e)=>{
+        e.preventDefault();
+        const formFilled = Object.values(formData).every(e=>e.length>0)
+        console.log("mailer",formFilled);
+        if(formFilled){
+            const date = new Date();
+            const obj = {
+                ...formData,
+                time:date
+            }
+            console.log("mailer data",obj)
+        }
+    }
+     const changeHandler = (e)=>{
+        const {value,name} = e.target;
+        const letters = /^[A-Za-z]+$/;
+        if(name==="userName"&&!letters.test(value))return
+    
+        setFormData(prev=>({
+            ...prev,
+            [name]:value
+        }))
+    
+    }
+  return (
+    <div className={styles.container}>
+        <span>Comment</span>
+      
+    <form onSubmit={(e)=>submitHandler(e,formData)}>
+    <input type="text" name="userName" placeholder='Name' onChange={(e)=>changeHandler(e,setFormData)} value={formData.userName}/>
+    <textarea type="text" name="userComment" placeholder='Comment' onChange={(e)=>changeHandler(e,setFormData)} value={formData.userComment}/>
+     <button>Post</button>
+    </form>
+     </div>
+  )
+}
+
+export default CommentCreationBox
